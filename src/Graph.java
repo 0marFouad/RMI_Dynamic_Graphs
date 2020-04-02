fe65f84ef58dbebc5767a120c02c9ae500cae68f
OmarFouad <omar.ahmed.fouad1996@gmail.com>
2020-04-02 11:17:57 +0200

    Done with Graph

--- src/Graph.java
+++ src/Graph.java
@@ -1,25 +1,24 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Scanner;
+import java.util.*;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class Graph implements GraphInterface {
-    private ArrayList <ArrayList <Integer>> graph;
+    private ArrayList <HashSet <Integer>> graph;
     private  String graphEnd = "S";
     private HashMap <Integer , Integer> nodeMap;
+    private static ReentrantLock lock;
 
     private int  getIndex(int node){
         if(nodeMap.containsKey(node)){
-           return nodeMap.get(node);
-
+            return nodeMap.get(node);
         }
         return createIndex(node);
     }
 
     private int createIndex(int node){
         int index = graph.size();
-        graph.add(new ArrayList<Integer>());
+        graph.add(new HashSet<>());
         nodeMap.put(node , index);
         return index;
     }
@@ -47,25 +46,70 @@ public class Graph implements GraphInterface {
             add(from,to);
         }
     }
-    public Graph() throws FileNotFoundException {
+    public Graph(Boolean isFile) throws FileNotFoundException {
+        lock = new ReentrantLock();
         nodeMap = new HashMap <>();
         graph = new ArrayList<>();
-        readFromStandardInput();
+        if(isFile){
+            readFromFile();
+        }else{
+            readFromStandardInput();
+        }
         System.out.println(graph);
         System.out.println("R\n");
     }
-    @Override
-    public int query(int from, int to) {
+
+    private int BFS(int from, int to){
+        Queue<Integer> q = new LinkedList<>();
+        q.add(from);
+        int height = 0;
+        HashSet<Integer> visited = new HashSet<>();
+        visited.add(from);
+        while(!q.isEmpty()){
+            int size = q.size();
+            for(int i=0;i<size;i++){
+                int x = q.poll();
+                if(x == to){
+                    return height;
+                }
+                Iterator<Integer> itr = graph.get(getIndex(x)).iterator();
+                while(itr.hasNext()){
+                    int new_node = itr.next();
+                    if(!visited.contains(new_node)){
+                        q.add(new_node);
+                        visited.add(new_node);
+                    }
+                }
+            }
+            height++;
+        }
         return 0;
     }
 
     @Override
-    public void delete(int from, int to) {
+    public int query(int from, int to) {
+        if(!nodeMap.containsKey(from) || !nodeMap.containsKey(to)){
+            return 0;
+        }
+        return BFS(from, to);
+    }
 
+    @Override
+    public void delete(int from, int to) {
+        lock.lock();
+        if(nodeMap.containsKey(from) && nodeMap.containsKey(to)){
+            int idx = nodeMap.get(from);
+            graph.get(idx).remove(to);
+        }
+        lock.unlock();
     }
 
     @Override
     public void add(int from, int to) {
-
+        lock.lock();
+        int idx_from = getIndex(from);
+        getIndex(to);
+        graph.get(idx_from).add(to);
+        lock.unlock();
     }
 }
--- src/GraphInterface.java
+++ src/GraphInterface.java
@@ -2,5 +2,4 @@ public interface GraphInterface {
     int query(int from, int to);
     void delete(int from, int to);
     void add(int from , int to);
-
 }
