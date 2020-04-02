23811400dc36907cb65a1771f33f3e892da3b235
safy97 <mostafasafy1997@gmail.com>
2020-04-02 09:01:42 +0200

    project architecture

---
+++ src/Graph.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+public class Graph implements GraphInterface {
+    ArrayList <ArrayList <Integer>> graph;
+    public Graph(){
+
+    }
+    @Override
+    public int query(int from, int to) {
+        return 0;
+    }
+
+    @Override
+    public void delete(int from, int to) {
+
+    }
+
+    @Override
+    public void add(int from, int to) {
+
+    }
+}
